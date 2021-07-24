9864f2fcae9c91b003be7dacb0e19f01892e000a
Joshua Oren <joshuadavidoren@gmail.com>
2021-07-24 11:23:38 -0500

    created table for movie inputs

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -21,7 +21,28 @@ function AddMovieForm() {
 
 return (
 
-
+<section>
+    <center>
+        <form action="submit">
+    <table>
+        <thead>
+        <tr><td><h2>Add Movie to Library</h2></td></tr>
+    </thead>
+    <tbody>
+    <tr><td>Name</td></tr>
+    <tr><td><input type="text" placeholder='Movie Title' onChange={(evt) => setTitle(evt.target.value)}/></td></tr>
+    <tr><td>Image</td></tr>
+    <tr><td><input type="text" placeholder='Insert Movie Image URL Here' onChange={(evt) => setURL(evt.target.value)}/></td></tr>
+    <tr><td>Description</td></tr>
+    <tr><td><textarea placeholder='Insert Movie Description Here' rows="4" cols="50" onChange={(evt) => setDescription(evt.target.value)}></textarea></td></tr>
+    </tbody>
+    <tfoot>
+        <tr><td><button type='submit' onClick={() => addMovieData()}>Submit</button></td></tr>
+    </tfoot>
+    </table>
+    </form>
+    </center>
+</section>
 
 )
 }
